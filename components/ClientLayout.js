d9835d21dd7fb251d5dda48c27645839a38d91d8
Mamun128169 <mh4559641@gmail.com>
2025-05-25 11:46:51 +0600

    error

--- components/ClientLayout.js
+++ components/ClientLayout.js
@@ -1,12 +1,16 @@
-import Navigation from "./Navigation";
+// components/ClientLayout.jsx
+"use client";
+
+import { AuthProvider } from "@/context/AuthContext"; // adjust the path
+import Navigation from "./Navigation"; // or wherever your nav is
 import Footer from "./Footer";
 
 export default function ClientLayout({ children }) {
   return (
-    <>
+    <AuthProvider>
       <Navigation />
       {children}
       <Footer />
-    </>
+    </AuthProvider>
   );
 }
